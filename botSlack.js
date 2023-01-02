e9b5811c8980d64b9778052b6cdf9f6ce80b0edb
Aximsoft <naveenchandar@gmail.com>
2023-01-02 15:19:58 +0530

    hardcoded bot token 2

--- botSlack.js
+++ botSlack.js
@@ -1,17 +1,20 @@
 // This is the main file for the starter-slack bot.
-const { Botkit } = require('botkit');
-const { BotkitCMSHelper } = require('./src/config/plugins/botkitCMS');
+const { Botkit } = require("botkit");
+const { BotkitCMSHelper } = require("./src/config/plugins/botkitCMS");
 
 // Import a platform-specific adapter for slack.
 
-const { SlackAdapter, SlackMessageTypeMiddleware, SlackEventMiddleware } = require('botbuilder-adapter-slack');
+const {
+  SlackAdapter,
+  SlackMessageTypeMiddleware,
+  SlackEventMiddleware,
+} = require("botbuilder-adapter-slack");
 
-const { MongoDbStorage } = require('botbuilder-storage-mongodb');
-const { DBStorage } = require('./src/config/plugins/dbStorage');
+const { MongoDbStorage } = require("botbuilder-storage-mongodb");
+const { DBStorage } = require("./src/config/plugins/dbStorage");
 
 // Load process.env values from .env file
-require('dotenv').config();
-
+require("dotenv").config();
 
 let tokenCache = {};
 let userCache = {};
@@ -22,7 +25,10 @@ if (process.env.MONGO_URI) {
     url: process.env.MONGO_URI,
   });
 }
-
+console.log(
+  "bot token",
+  "xoxb-3240171063603-4585922425892-dCu1UQ6kzRawxTijLu2i4TGz"
+);
 
 const adapter = new SlackAdapter({
   // REMOVE THIS OPTION AFTER YOU HAVE CONFIGURED YOUR APP!
@@ -37,7 +43,7 @@ const adapter = new SlackAdapter({
 
   clientId: "3240171063603.4580451420693",
   clientSecret: "7853456a7bb656c22dfabc231868f233",
-  oauthVersion: 'v2',
+  oauthVersion: "v2",
 
   // functions required for retrieving team-specific info
   // for use in multi-team apps
@@ -51,53 +57,58 @@ adapter.use(new SlackEventMiddleware());
 // Use SlackMessageType middleware to further classify messages as direct_message, direct_mention, or mention
 adapter.use(new SlackMessageTypeMiddleware());
 
-
 const controller = new Botkit({
-  webhook_uri: '/api/messages',
+  webhook_uri: "/api/messages",
   adapter: adapter,
   storage,
 });
 
 if (process.env.MONGO_URI) {
-  controller.usePlugin(new DBStorage({
-    mongoUri: process.env.MONGO_URI,
-  }));
+  controller.usePlugin(
+    new DBStorage({
+      mongoUri: process.env.MONGO_URI,
+    })
+  );
 }
 
 if (process.env.CMS_URI) {
-  controller.usePlugin(new BotkitCMSHelper({
-    uri: process.env.CMS_URI,
-    token: process.env.CMS_TOKEN,
-  }));
+  controller.usePlugin(
+    new BotkitCMSHelper({
+      uri: process.env.CMS_URI,
+      token: process.env.CMS_TOKEN,
+    })
+  );
 }
 
 // Once the bot has booted up its internal services, you can use them to do stuff.
 controller.ready(() => {
-
   // load traditional developer-created local custom feature modules
-  controller.loadModules(__dirname + '/src/features');
+  controller.loadModules(__dirname + "/src/features");
 
   /* catch-all that uses the CMS to trigger dialogs */
   if (controller.plugins.cms) {
-    controller.on('message,direct_message,interactive_message', async (bot, message) => {
-      try {
-        new Promise(async () => {
-          await bot.changeContext(message.reference); // important or you will get a proxy error
-          return await controller.plugins.cms.testTrigger(bot, message);
-        });
-        return;
-      } catch (ex) {
-        console.log('exception in CMS trigger event --> ', ex);
+    controller.on(
+      "message,direct_message,interactive_message",
+      async (bot, message) => {
+        try {
+          new Promise(async () => {
+            await bot.changeContext(message.reference); // important or you will get a proxy error
+            return await controller.plugins.cms.testTrigger(bot, message);
+          });
+          return;
+        } catch (ex) {
+          console.log("exception in CMS trigger event --> ", ex);
+        }
       }
-    });
+    );
   }
 });
 
-controller.webserver.get('/', (req, res) => {
+controller.webserver.get("/", (req, res) => {
   res.send(`This app is running Botkit ${controller.version}.`);
 });
 
-controller.webserver.get('/install/auth', async (req, res) => {
+controller.webserver.get("/install/auth", async (req, res) => {
   try {
     // returns access token if OAuthCode validated successfully
     const results = await controller.adapter.validateOauthCode(req.query.code);
@@ -108,14 +119,13 @@ controller.webserver.get('/install/auth', async (req, res) => {
     // Capture team to bot id
     userCache[results.team_id] = results.bot_user_id;
 
-    res.json('Success! Bot installed.');
+    res.json("Success! Bot installed.");
   } catch (err) {
-    console.error('OAUTH ERROR:', err);
+    console.error("OAUTH ERROR:", err);
     res.status(401);
     res.send(err.message);
   }
 });
-
 
 if (process.env.TOKENS) {
   tokenCache = JSON.parse(process.env.TOKENS);
@@ -133,7 +143,7 @@ async function getTokenForTeam(teamId) {
       }, 150);
     });
   } else {
-    console.error('Team not found in tokenCache: ', teamId);
+    console.error("Team not found in tokenCache: ", teamId);
   }
 }
 
@@ -145,6 +155,6 @@ async function getBotUserByTeam(teamId) {
       }, 150);
     });
   } else {
-    console.error('Team not found in userCache: ', teamId);
+    console.error("Team not found in userCache: ", teamId);
   }
 }
